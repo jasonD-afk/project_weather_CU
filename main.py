05daaa17d7146dd4cb8542ba6a80361745ef4a85
David <d.sadoyan@edu.centraluniversity.ru>
2024-12-08 23:51:38 +0300

    add .env configuration

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import requests
 from pprint import pprint
-
+import os
+from dotenv import load_dotenv
+load_dotenv()
+api_keys = os.getenv("API_KEY")
 response = requests.get(
     url="http://dataservice.accuweather.com/locations/v1/cities/geoposition/search",
     params={
-        'apikey': "VWOEIIGDxX7dgDf1WvVKzsA5gsAw6qGy",
+        'apikey': api_keys,
         'q': '55.768760,37.588817',
         'language': 'ru-ru'
 
@@ -12,3 +15,4 @@ response = requests.get(
 
 )
 pprint(response.json())
+
