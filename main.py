d5a2b62440f1acf45d47d453c10ccd048ca66e5c
David <d.sadoyan@edu.centraluniversity.ru>
2024-12-09 15:46:22 +0300

    add task 2: vision temperature, wind spead, rain probiliri, himidity

--- main.py
+++ main.py
@@ -14,5 +14,23 @@ location_api = requests.get(
     }
 
 )
-pprint(location_api.json())
+location_key = location_api.json()["Key"]
 
+hour1_forecast_api = requests.get(
+    url=f"http://dataservice.accuweather.com/forecasts/v1/hourly/1hour/{location_key}",
+    params={
+        'apikey': api_keys,
+        "language": 'ru-ru',
+        'metric': 'true',
+        'details': 'true'
+    }
+)
+temperature = hour1_forecast_api.json()[0]['Temperature']['Value']
+
+
+humidity = hour1_forecast_api.json()[0]["RelativeHumidity"]
+
+wind_spead = hour1_forecast_api.json()[0]['Wind']['Speed']['Value']
+rain_probailiti = hour1_forecast_api.json()[0]["RainProbability"]
+print(f'температура: {temperature} {hour1_forecast_api.json()[0]['Temperature']['Unit']}\nвлажность: {humidity} %\nскорость ветра: {wind_spead} {hour1_forecast_api.json()[0]['Wind']['Speed']['Unit']},\nвероятность дождя: {rain_probailiti} %')
+# pprint(hour1_forecast_api.json())
